ddf70da3d9e002e027e1b20f69ae4329db38f507
Nico Stuurman <nico.stuurman@ucsf.edu>
2017-08-22 17:13:06 -0700

    UI more or less complete.

--- src/main/java/edu/ucsf/valelab/dispimfusionui/DiSpimFusion.java
+++ src/main/java/edu/ucsf/valelab/dispimfusionui/DiSpimFusion.java
@@ -41,11 +41,16 @@ public class DiSpimFusion implements PlugIn {
    public static final String IMAGEANAME = "Image A Name";
    public static final String IMAGEBNAME = "Image B Name";
    public static final String OUTPUTDIRECTORY = "Output Directory";
+   public static final String PSFA = "PSFA";
+   public static final String PSFB = "PSFB";
    // used as int inputs
    public static final String START = "Start #";
    public static final String END = "End #";
    public static final String INTERVAL = "Interval";
    public static final String TEST = "Test#";
+   public static final String DECONVOLUTIONITERATIONS = "Iterations";
+   public static final String OUTPUTBITS = "Output BitDepth";
+   public static final String GPUDEVICE = "GPU Device #";
    // used as double inputs
    public static final String XPIXELSIZEA = "ImageA x";
    public static final String YPIXELSIZEA = "ImageA y";
@@ -62,6 +67,10 @@ public class DiSpimFusion implements PlugIn {
    public static final String CUSTOMIZEMATRIX = "Customize initial transformation matrix";
    public static final String SAVEREGISTEREDIMAGES = "Save Registered Images";
    public static final String SHOWGPUINFO = "Show GPU Device Information";
+   
+   // miglayout options
+   private final String paragraphSpacing_ = "wrap 15px";
+   private final String spinnerWidth_ = "w 60:60:60";
    
    final private Preferences prefs_;
 
@@ -81,26 +90,27 @@ public class DiSpimFusion implements PlugIn {
    
    private final String[] textFields_ = {
          SPIMADIRECTORY, SPIMBDIRECTORY, IMAGEANAME,  IMAGEBNAME, 
-         OUTPUTDIRECTORY };
-   private final Map jTextFields_ = 
+         OUTPUTDIRECTORY, PSFA, PSFB };
+   private final Map<String, JTextField> jTextFields_ = 
            new HashMap<String, JTextField>(textFields_.length);
    
    private final String[] intSpinnerLabels_ = {
-         START, END, INTERVAL, TEST };
-   private final Map intSpinners_ =
+         START, END, INTERVAL, TEST, DECONVOLUTIONITERATIONS, 
+         OUTPUTBITS, GPUDEVICE };
+   private final Map<String, JSpinner> intSpinners_ =
            new HashMap<String, JSpinner>(intSpinnerLabels_.length);
    
    private final String[] doubleSpinnerLabels_ = {
          XPIXELSIZEA, YPIXELSIZEA, ZPIXELSIZEA, 
          XPIXELSIZEB, YPIXELSIZEB, ZPIXELSIZEB,
          CONVERGENCETHRESHOLD };
-   private final Map  doubleSpinners_ = 
+   private final Map<String, JSpinner>  doubleSpinners_ = 
            new HashMap<String, JSpinner>(doubleSpinnerLabels_.length);
    
    private final String[] checkBoxLabels_ = {
       DO2DREGISTRATION , CUSTOMIZEMATRIX, 
       SAVEREGISTEREDIMAGES, SHOWGPUINFO };
-   private final Map checkBoxes_ = 
+   private final Map<String, JCheckBox> checkBoxes_ = 
            new HashMap<String, JCheckBox>(checkBoxLabels_.length);
    
    public DiSpimFusion () {
@@ -117,13 +127,19 @@ public class DiSpimFusion implements PlugIn {
       }
       ((JSpinner) intSpinners_.get(END)).setModel(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
       ((JSpinner) intSpinners_.get(INTERVAL)).setModel(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
+      ((JSpinner) intSpinners_.get(OUTPUTBITS)).setModel(new SpinnerNumberModel(16, 16, 32, 16));
       
       for (String dsp : doubleSpinnerLabels_) {
          doubleSpinners_.put(dsp, new JSpinner(
                  new SpinnerNumberModel(0.165, 0.010, 10.0, 0.001)));
       }
       ((JSpinner) doubleSpinners_.get(CONVERGENCETHRESHOLD)).setModel(
-              new SpinnerNumberModel(0.001, 0.0, 1.0, 0.00001));
+              new SpinnerNumberModel(0.0001, 0.0, 1.0, 0.00001));
+      // There seems to be a bug or feature in JSpinner that does not like to show a large number of digits
+      // work around that by explicitly setting the max Fraction digits in the editor
+      JSpinner.NumberEditor editor = 
+              (JSpinner.NumberEditor)((JSpinner) doubleSpinners_.get(CONVERGENCETHRESHOLD)).getEditor();
+      editor.getFormat().setMaximumFractionDigits(7);
       
       for (String cb : checkBoxLabels_) {
          checkBoxes_.put(cb, new JCheckBox(cb));
@@ -159,75 +175,75 @@ public class DiSpimFusion implements PlugIn {
       ""  // row constraints
       ));
       
-      addDirectoryChoice(frame, optionsString, SPIMADIRECTORY);
-      addDirectoryChoice(frame, optionsString, SPIMBDIRECTORY);
+      addFDChoice(frame, optionsString, SPIMADIRECTORY, true);
+      addFDChoice(frame, optionsString, SPIMBDIRECTORY, true);
       
       frame.add(new Label(IMAGEANAME));
-      JTextField tmp = (JTextField) jTextFields_.get(IMAGEANAME);
+      JTextField tmp = jTextFields_.get(IMAGEANAME);
       tmp.setText(Macro.getValue(optionsString, IMAGEANAME.replaceAll("\\s",""), 
               prefs_.get(IMAGEANAME, "SPIMA_")));
-      frame.add(tmp, "growx");
+      frame.add(tmp, "w 120:120:120");
       frame.add(new Label(IMAGEBNAME), "gapleft push");
-      JTextField tmp2 = (JTextField) jTextFields_.get(IMAGEBNAME);
+      JTextField tmp2 = jTextFields_.get(IMAGEBNAME);
       tmp2.setText(Macro.getValue(optionsString, IMAGEBNAME.replaceAll("\\s",""), 
               prefs_.get(IMAGEBNAME, "SPIMB_")));
-      frame.add(tmp2, "growx, wrap");
+      frame.add(tmp2, "w 120:120:120, wrap");
       
-      addDirectoryChoice(frame, optionsString, OUTPUTDIRECTORY);
+      addFDChoice(frame, optionsString, OUTPUTDIRECTORY, true);
       
       frame.add(new Label(START));
-      ((JSpinner) intSpinners_.get(START)).setValue(Integer.parseInt( Macro.getValue(optionsString, START.replaceAll("\\s", ""), 
+      intSpinners_.get(START).setValue(Integer.parseInt( Macro.getValue(optionsString, START.replaceAll("\\s", ""), 
                Integer.toString(prefs_.getInt(START, 0))) ));
-      frame.add(((JSpinner) intSpinners_.get(START)), "w 50:50:50");
+      frame.add(intSpinners_.get(START), spinnerWidth_);
       frame.add(new Label(INTERVAL), "right");
-      ((JSpinner) intSpinners_.get(INTERVAL)).setValue(Integer.parseInt( Macro.getValue(optionsString, INTERVAL.replaceAll("\\s", ""), 
+      intSpinners_.get(INTERVAL).setValue(Integer.parseInt( Macro.getValue(optionsString, INTERVAL.replaceAll("\\s", ""), 
                Integer.toString(prefs_.getInt(INTERVAL, 1))) ));
-      frame.add(((JSpinner) intSpinners_.get(INTERVAL)), "w 50:50:50, wrap");
+      frame.add(intSpinners_.get(INTERVAL), spinnerWidth_ +", wrap");
       frame.add(new Label(END));
-      ((JSpinner) intSpinners_.get(END)).setValue(Integer.parseInt( Macro.getValue(optionsString, END.replaceAll("\\s", ""), 
+      intSpinners_.get(END).setValue(Integer.parseInt( Macro.getValue(optionsString, END.replaceAll("\\s", ""), 
                Integer.toString(prefs_.getInt(END, 1))) ));
-      frame.add(((JSpinner) intSpinners_.get(END)), "w 50:50:50");
+      frame.add(intSpinners_.get(END), spinnerWidth_);
       String testToolTip = "When registration option is set to \"One image only\", " +
                            "this image will be used. ";
       frame.add(new Label(TEST), "right");
-      ((JSpinner) intSpinners_.get(TEST)).setValue(Integer.parseInt( Macro.getValue(optionsString, TEST.replaceAll("\\s", ""), 
+      intSpinners_.get(TEST).setValue(Integer.parseInt( Macro.getValue(optionsString, TEST.replaceAll("\\s", ""), 
                Integer.toString(prefs_.getInt(TEST, 0))) ));
-      ((JSpinner) intSpinners_.get(TEST)).setToolTipText(testToolTip);
-      frame.add(((JSpinner) intSpinners_.get(TEST)), "w 50:50:50, wrap 15px");
+      intSpinners_.get(TEST).setToolTipText(testToolTip);
+      frame.add(((JSpinner) intSpinners_.get(TEST)), spinnerWidth_ + ", " + paragraphSpacing_);
       
       frame.add(new Label("Initial Pixel Sizes (in microns)"), "span 4, align left, wrap");
       
       frame.add(new Label (XPIXELSIZEA));
-      ((JSpinner) doubleSpinners_.get(XPIXELSIZEA)).setValue(Double.parseDouble(Macro.getValue(optionsString, 
+      doubleSpinners_.get(XPIXELSIZEA).setValue(Double.parseDouble(Macro.getValue(optionsString, 
               XPIXELSIZEA.replaceAll("\\s", ""),
               Double.toString(prefs_.getDouble(XPIXELSIZEA, 0.165)))));
       frame.add(((JSpinner) doubleSpinners_.get(XPIXELSIZEA)), "span 3, split 5, w 50");
       frame.add(new Label("y"), "gapleft push");
-      ((JSpinner) doubleSpinners_.get(YPIXELSIZEA)).setValue(Double.parseDouble(Macro.getValue(optionsString, 
+      doubleSpinners_.get(YPIXELSIZEA).setValue(Double.parseDouble(Macro.getValue(optionsString, 
               YPIXELSIZEA.replaceAll("\\s", ""),
               Double.toString(prefs_.getDouble(YPIXELSIZEA, 0.165)))));
-      frame.add(((JSpinner) doubleSpinners_.get(YPIXELSIZEA)));
+      frame.add(doubleSpinners_.get(YPIXELSIZEA));
       frame.add(new Label("z"), "gapleft push");
-      ((JSpinner) doubleSpinners_.get(ZPIXELSIZEA)).setValue(Double.parseDouble(Macro.getValue(optionsString, 
+      doubleSpinners_.get(ZPIXELSIZEA).setValue(Double.parseDouble(Macro.getValue(optionsString, 
               ZPIXELSIZEA.replaceAll("\\s", ""),
               Double.toString(prefs_.getDouble(ZPIXELSIZEA, 1.0)))));
-      frame.add(((JSpinner) doubleSpinners_.get(ZPIXELSIZEA)), "wrap");
+      frame.add(doubleSpinners_.get(ZPIXELSIZEA), "wrap");
       
       frame.add(new Label (XPIXELSIZEB));
-      ((JSpinner) doubleSpinners_.get(XPIXELSIZEB)).setValue(Double.parseDouble(Macro.getValue(optionsString, 
+      doubleSpinners_.get(XPIXELSIZEB).setValue(Double.parseDouble(Macro.getValue(optionsString, 
               XPIXELSIZEB.replaceAll("\\s", ""),
               Double.toString(prefs_.getDouble(XPIXELSIZEB, 0.165)))));
-      frame.add(((JSpinner) doubleSpinners_.get(XPIXELSIZEB)), "span 3, split 5, w 50");
+      frame.add(doubleSpinners_.get(XPIXELSIZEB), "span 3, split 5, w 50");
       frame.add(new Label("y"), "gapleft push");
-      ((JSpinner) doubleSpinners_.get(YPIXELSIZEB)).setValue(Double.parseDouble(Macro.getValue(optionsString, 
+      doubleSpinners_.get(YPIXELSIZEB).setValue(Double.parseDouble(Macro.getValue(optionsString, 
               YPIXELSIZEB.replaceAll("\\s", ""),
               Double.toString(prefs_.getDouble(YPIXELSIZEB, 0.165)))));
-      frame.add(((JSpinner) doubleSpinners_.get(YPIXELSIZEB)));
+      frame.add(doubleSpinners_.get(YPIXELSIZEB));
       frame.add(new Label("z"), "gapleft push");
-      ((JSpinner) doubleSpinners_.get(ZPIXELSIZEB)).setValue(Double.parseDouble(Macro.getValue(optionsString, 
+      doubleSpinners_.get(ZPIXELSIZEB).setValue(Double.parseDouble(Macro.getValue(optionsString, 
               ZPIXELSIZEB.replaceAll("\\s", ""),
               Double.toString(prefs_.getDouble(ZPIXELSIZEB, 1.0)))));
-      frame.add(((JSpinner) doubleSpinners_.get(ZPIXELSIZEB)), "wrap 15px");
+      frame.add(doubleSpinners_.get(ZPIXELSIZEB), paragraphSpacing_);
       
       frame.add(new Label("Registration Options"));
       registrationOptions_.setSelectedItem(Macro.getValue(optionsString,
@@ -239,19 +255,19 @@ public class DiSpimFusion implements PlugIn {
       imageBRotation_.setSelectedItem(Macro.getValue(optionsString,
               IMAGEBROTATION.replaceAll("\\s", ""),
               prefs_.get(IMAGEBROTATION, imageBRotationsStr_[2])));
-      frame.add(imageBRotation_, "span 3, alig left, wrap 15px");
+      frame.add(imageBRotation_, "span 3, alig left, " + paragraphSpacing_);
       
-      ((JCheckBox) checkBoxes_.get(DO2DREGISTRATION)).setSelected(
+      checkBoxes_.get(DO2DREGISTRATION).setSelected(
                Boolean.parseBoolean(
                   Macro.getValue(optionsString, DO2DREGISTRATION.replaceAll("\\s", ""),
                   String.valueOf(prefs_.getBoolean(DO2DREGISTRATION, false)))));
-      frame.add(((JCheckBox) checkBoxes_.get(DO2DREGISTRATION)), "span 2, left");
+      frame.add(checkBoxes_.get(DO2DREGISTRATION), "span 2, left");
        
-      ((JCheckBox) checkBoxes_.get(SAVEREGISTEREDIMAGES)).setSelected(
+      checkBoxes_.get(SAVEREGISTEREDIMAGES).setSelected(
                Boolean.parseBoolean(
                   Macro.getValue(optionsString, SAVEREGISTEREDIMAGES.replaceAll("\\s", ""),
                   String.valueOf(prefs_.getBoolean(SAVEREGISTEREDIMAGES, false)))));
-      frame.add(((JCheckBox) checkBoxes_.get(SAVEREGISTEREDIMAGES)), "span 2, left, wrap");
+      frame.add(checkBoxes_.get(SAVEREGISTEREDIMAGES), "span 2, left, wrap");
         
       ((JCheckBox) checkBoxes_.get(CUSTOMIZEMATRIX)).setSelected(
                Boolean.parseBoolean(
@@ -259,17 +275,37 @@ public class DiSpimFusion implements PlugIn {
                   String.valueOf(prefs_.getBoolean(CUSTOMIZEMATRIX, false)))));
       frame.add(((JCheckBox) checkBoxes_.get(CUSTOMIZEMATRIX)), "span3, left, wrap");
 
-      frame.add(new Label(CONVERGENCETHRESHOLD));
-     // ((JSpinner) doubleSpinners_.get(CONVERGENCETHRESHOLD)).setValue(Double.parseDouble(Macro.getValue(optionsString, 
-     //         CONVERGENCETHRESHOLD.replaceAll("\\s", ""),
-     //         Double.toString(prefs_.getDouble(CONVERGENCETHRESHOLD, 0.00001)))));
-      frame.add(((JSpinner) doubleSpinners_.get(CONVERGENCETHRESHOLD)), "span 2, growx, wrap 15px");
+      frame.add(new Label(CONVERGENCETHRESHOLD), "span 2, left");
+      doubleSpinners_.get(CONVERGENCETHRESHOLD).setValue(Double.parseDouble(Macro.getValue(optionsString, 
+              CONVERGENCETHRESHOLD.replaceAll("\\s", ""),
+              Double.toString(prefs_.getDouble(CONVERGENCETHRESHOLD, 0.00001)))));
+      frame.add(doubleSpinners_.get(CONVERGENCETHRESHOLD), "growx, " + paragraphSpacing_);
       
-      ((JCheckBox) checkBoxes_.get(SHOWGPUINFO)).setSelected(
+      frame.add(new Label("Deconvolution Options"), "span 4, left, wrap");
+      addFDChoice(frame, optionsString, PSFA, false);
+      addFDChoice(frame, optionsString, PSFB, false);
+      intSpinners_.get(DECONVOLUTIONITERATIONS).setValue(Integer.parseInt(Macro.getValue(
+               optionsString, DECONVOLUTIONITERATIONS.replaceAll("\\s", ""),
+               Integer.toString(prefs_.getInt(DECONVOLUTIONITERATIONS, 10)))));
+      frame.add(new Label(DECONVOLUTIONITERATIONS));
+      frame.add(intSpinners_.get(DECONVOLUTIONITERATIONS), spinnerWidth_);
+      intSpinners_.get(OUTPUTBITS).setValue(Integer.parseInt(Macro.getValue(
+              optionsString, OUTPUTBITS.replaceAll("\\s", ""), 
+              Integer.toString(prefs_.getInt(OUTPUTBITS, 16)))));
+      frame.add(new Label(OUTPUTBITS));
+      frame.add(intSpinners_.get(OUTPUTBITS), spinnerWidth_ + ", " + paragraphSpacing_);
+
+      frame.add(new Label("GPU Options"), "span 4, left, wrap");
+      checkBoxes_.get(SHOWGPUINFO).setSelected(
                Boolean.parseBoolean(
                   Macro.getValue(optionsString, SHOWGPUINFO.replaceAll("\\s", ""),
                   String.valueOf(prefs_.getBoolean(SHOWGPUINFO, false)))));
-      frame.add(((JCheckBox) checkBoxes_.get(SHOWGPUINFO)), "span3, left, wrap");
+      frame.add(checkBoxes_.get(SHOWGPUINFO), "span3, left, wrap");
+      intSpinners_.get(GPUDEVICE).setValue(Integer.parseInt(Macro.getValue(
+              optionsString, GPUDEVICE.replaceAll("\\s", ""), 
+              Integer.toString(prefs_.getInt(GPUDEVICE, 0)))));
+      frame.add(new Label(GPUDEVICE));
+      frame.add(intSpinners_.get(GPUDEVICE), spinnerWidth_ + ", " + paragraphSpacing_);
       
       JButton okButton = new JButton("OK");
       okButton.addActionListener(new ActionListener(){
@@ -315,13 +351,25 @@ public class DiSpimFusion implements PlugIn {
       return button;
    }
    
-   private void addDirectoryChoice(JFrame frame, String optionsString, String identifier) {        
+   /**
+    * Creates a row in the given frame with a label, textfield, and button
+    * that the user may use to place a file or directory in the textfield
+    * @param frame Frame to which the row will be added
+    * @param optionsString OptionsString as passed by an ImageJ macro
+    * @param identifier Key to this field, will be used as label
+    * @param directory directory when true, file otherwise
+    */
+   private void addFDChoice(
+           final JFrame frame, 
+           final String optionsString, 
+           final String identifier,
+           final boolean directory) {        
       frame.add(new Label(identifier));
       JTextField tmp = (JTextField)jTextFields_.get(identifier);
       frame.add(tmp, "span 3, split 2, width 250, growx");
       tmp.setText(Macro.getValue(optionsString, identifier.replaceAll("\\s",""), 
               prefs_.get(identifier, "")));
-      frame.add(chooserButton(identifier, tmp, true), "wrap");   
+      frame.add(chooserButton(identifier, tmp, directory), "wrap");   
    }
  
    /**
@@ -377,4 +425,5 @@ public class DiSpimFusion implements PlugIn {
          Recorder.saveCommand();
       }
    }
+   
 }
